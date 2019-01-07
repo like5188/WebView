95de98e57526cd3337ebe9e52d837497ca8ab056
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2019-01-07 15:52:54 +0800

    新版webView

--- sample/src/main/java/com/like/webview/sample/WebViewActivity.kt
+++ sample/src/main/java/com/like/webview/sample/WebViewActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.like.webview.X5Listener
+import com.like.webview.X5ProgressBarWebView
 import com.like.webview.X5WebView
 import com.like.webview.sample.databinding.ActivityWebviewBinding
 import com.tencent.smtt.sdk.WebSettings
@@ -15,20 +16,20 @@ class WebViewActivity : AppCompatActivity() {
     private val mBinding: ActivityWebviewBinding by lazy {
         DataBindingUtil.setContentView<ActivityWebviewBinding>(this, R.layout.activity_webview)
     }
-    private val x5WebView: X5WebView by lazy {
-        mBinding.webView.x5WebView
+    private val x5ProgressBarWebView: X5ProgressBarWebView by lazy {
+        mBinding.webView
     }
     private val webView: WebView by lazy {
-        x5WebView.tencentWebView
+        x5ProgressBarWebView.x5WebView.tencentWebView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         webView.addJavascriptInterface(JavascriptObject(webView), "androidAPI")
-        val url = "file:///android_asset/index.html"
-//        val url = "http://www.sohu.com/"
+//        val url = "file:///android_asset/index.html"
+        val url = "http://www.sohu.com/"
         webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE// 支持微信H5支付
-        x5WebView.mListener = object : X5Listener {
+        x5ProgressBarWebView.mListener = object : X5Listener {
             override fun onReceivedIcon(webView: WebView?, icon: Bitmap?) {
                 mBinding.ivIcon.setImageBitmap(icon)
             }
