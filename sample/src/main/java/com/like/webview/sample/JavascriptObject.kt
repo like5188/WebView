510f771b084ac966c9b45203981e01d10038ed07
like <like@adtech.com.cn>
2019-03-30 11:06:39 +0800

    完善

--- sample/src/main/java/com/like/webview/sample/JavascriptObject.kt
+++ sample/src/main/java/com/like/webview/sample/JavascriptObject.kt
@@ -7,11 +7,11 @@ class JavascriptObject(private val webView: WebView) {
 
     // js调用android方法
     @JavascriptInterface // API17及以上的版本中，需要此注解才能调用下面的方法
-    fun gotoOrderConfirm(goods_list: String, activity_type: Int) {
+    fun handleJs(goods_list: String, activity_type: Int) {
     }
 
     // android调用js方法
-    fun updateImage(position: Int, imagePath: String) {
+    fun callJs(position: Int, imagePath: String) {
         val jsString = "javascript:methodName($position,'$imagePath')"
         webView.post { webView.loadUrl(jsString) }
         // a)比第一种方法效率更高、使用更简洁，因为该方法的执行不会使页面刷新，而第一种方法（loadUrl ）的执行则会。
