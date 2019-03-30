03cb609a76da94ecc05f3484e7e2d7574f8c0335
like <like@adtech.com.cn>
2019-03-30 11:10:19 +0800

    完善

--- sample/src/main/java/com/like/webview/sample/JavascriptObject.kt
+++ sample/src/main/java/com/like/webview/sample/JavascriptObject.kt
@@ -7,13 +7,14 @@ class JavascriptObject(private val webView: WebView) {
 
     // js调用android方法
     @JavascriptInterface // API17及以上的版本中，需要此注解才能调用下面的方法
-    fun handleJs(goods_list: String, activity_type: Int) {
+    fun handleJS(goods_list: String, activity_type: Int): String {
+        return ""
     }
 
     // android调用js方法
-    fun callJs(position: Int, imagePath: String) {
-        val jsString = "javascript:methodName($position,'$imagePath')"
-        webView.post { webView.loadUrl(jsString) }
+    fun callJS(position: Int, imagePath: String) {
+        val jsString = "javascript:jsMethodName($position,'$imagePath')"
+        webView.post { webView.loadUrl(jsString) }// Ui线程
         // a)比第一种方法效率更高、使用更简洁，因为该方法的执行不会使页面刷新，而第一种方法（loadUrl ）的执行则会。
         // b)Android 4.4 后才可使用
         webView.evaluateJavascript(jsString) {
