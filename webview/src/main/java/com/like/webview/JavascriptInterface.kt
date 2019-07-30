1bb2f1ac5982547354ddfd58c8dd56f2e0f067c5
like <like@adtech.com.cn>
2019-07-30 13:30:38 +0800

    完善注释

--- webview/src/main/java/com/like/webview/JavascriptInterface.kt
+++ webview/src/main/java/com/like/webview/JavascriptInterface.kt
@@ -8,13 +8,26 @@ class JavascriptInterface(private val webView: WebView) {
     private val androidMethodMap = mutableMapOf<String, (String) -> String>()
 
     /**
-     * 注册方法。这个方法会被JS调用。
+     * 注册 android 方法。这个方法会被JS调用。
+     * js 调用如下：
+     * function callAndroid(){// 方法名字随意
+     *      var user = {};
+     *      user.name= "like";
+     *      user.age = 36;
+     *      var str=window.androidAPI.callAndroidMethod("androidMethodName",JSON.stringify(user));// window.androidAPI.callAndroidMethod(methodName, paramsJsonString) 为固定格式
+     *      alert("调用了Android的callAndroidMethod方法，返回值：" + str);
+     * }
+     *
+     * @param methodName    android 方法的名字
+     * @param method        android 方法。传入方法的参数是 String 类型，如果是多个参数，可以用 json 字符串；返回值也是 String 类型的
      */
     fun registerAndroidMethodForJSCall(methodName: String, method: (String) -> String) {
         androidMethodMap[methodName] = method
     }
 
-    // js调用android方法
+    /**
+     * js 调用 android 方法
+     */
     @JavascriptInterface // API17及以上的版本中，需要此注解才能调用下面的方法
     fun callAndroidMethod(methodName: String, paramsJsonString: String): String {
         return if (androidMethodMap.containsKey(methodName)) {
@@ -27,7 +40,13 @@ class JavascriptInterface(private val webView: WebView) {
         }
     }
 
-    // android调用js方法
+    /**
+     * android 调用 js 方法
+     *
+     * @param jsMethodName      js 方法的名字
+     * @param paramsJsonString  js 方法的参数
+     * @param callback          回调方法，用于处理 js 方法返回的 String 类型的结果。
+     */
     fun callJsMethod(jsMethodName: String, paramsJsonString: String? = null, callback: ((String) -> Unit)? = null) {
         if (jsMethodName.isEmpty()) return
         val jsString = if (paramsJsonString.isNullOrEmpty()) {
