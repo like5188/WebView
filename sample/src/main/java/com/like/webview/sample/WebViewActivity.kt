6bb126b1b1df79323d1fdcd886a4f65277c6d159
like <like@adtech.com.cn>
2019-08-06 10:53:15 +0800

    迁移到androidx

--- sample/src/main/java/com/like/webview/sample/WebViewActivity.kt
+++ sample/src/main/java/com/like/webview/sample/WebViewActivity.kt
@@ -1,9 +1,9 @@
 package com.like.webview.sample
 
-import android.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import com.like.webview.JavascriptInterface
