f40a2ca699d08e12f534c108248b866d61062070
like5188 <260864866@qq.com>
2022-02-14 15:34:34 +0800

    内存泄漏问题

--- sample/src/main/java/com/like/webview/sample/viewpager/ViewPagerAdapter.kt
+++ sample/src/main/java/com/like/webview/sample/viewpager/ViewPagerAdapter.kt
@@ -3,7 +3,6 @@ package com.like.webview.sample.viewpager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.like.webview.sample.WebViewFragment
 
 class ViewPagerAdapter(private val fragments: List<Fragment>, fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
@@ -12,8 +11,7 @@ class ViewPagerAdapter(private val fragments: List<Fragment>, fragmentActivity: 
     }
 
     override fun createFragment(position: Int): Fragment {
-//        return Fragment1()
-        return WebViewFragment("https://www.baidu.com/")
+        return fragments[position]
     }
 
 }
