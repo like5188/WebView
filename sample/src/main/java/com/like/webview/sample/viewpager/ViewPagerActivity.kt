a839ac56a2bf6ccb4e32bf30da9574aa37e963fe
like5188 <260864866@qq.com>
2022-02-14 14:06:23 +0800

    完善例子

--- sample/src/main/java/com/like/webview/sample/viewpager/ViewPagerActivity.kt
+++ sample/src/main/java/com/like/webview/sample/viewpager/ViewPagerActivity.kt
@@ -14,37 +14,30 @@ class ViewPagerActivity : FragmentActivity() {
     }
     private val fragments = listOf(
         WebViewFragment("https://www.baidu.com/"),
-        Fragment2(),
-        Fragment3(),
-        Fragment4()
+        WebViewFragment("https://www.baidu.com/"),
+        WebViewFragment("https://www.baidu.com/"),
+        WebViewFragment("https://www.baidu.com/")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding
-//        addFragments(R.id.fragment_holder, 0, *fragments.toTypedArray())
-
         mBinding.vp.adapter = ViewPagerAdapter(fragments, this)
         mBinding.vp.offscreenPageLimit = fragments.size - 1// 避免fragment被销毁导致重新懒加载
     }
 
     fun showFragment1(view: View) {
-//        showFragment(fragments[0])
         mBinding.vp.setCurrentItem(0, false)
     }
 
     fun showFragment2(view: View) {
-//        showFragment(fragments[1])
         mBinding.vp.setCurrentItem(1, false)
     }
 
     fun showFragment3(view: View) {
-//        showFragment(fragments[2])
         mBinding.vp.setCurrentItem(2, false)
     }
 
     fun showFragment4(view: View) {
-//        showFragment(fragments[3])
         mBinding.vp.setCurrentItem(3, false)
     }
 
