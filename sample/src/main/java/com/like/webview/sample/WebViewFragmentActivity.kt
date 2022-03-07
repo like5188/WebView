02acbd280c1d3671c4fd21f94d6bde570c012e20
like5188 <260864866@qq.com>
2022-03-07 18:29:50 +0800

    完善

--- sample/src/main/java/com/like/webview/sample/WebViewFragmentActivity.kt
+++ sample/src/main/java/com/like/webview/sample/WebViewFragmentActivity.kt
@@ -4,11 +4,13 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.like.common.util.Logger
 import com.like.webview.BaseWebViewActivity
 import com.like.webview.WebViewFragmentConfig
 import com.like.webview.X5ListenerAdapter
 import com.like.webview.sample.databinding.ActivityWebviewFragmentBinding
 import com.tencent.smtt.sdk.WebView
+import org.json.JSONObject
 
 class WebViewFragmentActivity : BaseWebViewActivity() {
     private val mBinding by lazy {
@@ -26,6 +28,7 @@ class WebViewFragmentActivity : BaseWebViewActivity() {
 
     override fun getWebViewFragmentConfig(): WebViewFragmentConfig = WebViewFragmentConfig().apply {
         url = "file:///android_asset/index.html"
+//        url = "http://kcwc.m.cacf.cn/static-kcwc2/my/userInfo?client=android"
         javascriptInterfaceMap["appKcwc"] = JsUtils.JavascriptInterface()
         x5Listener = object : X5ListenerAdapter() {
             override fun onReceivedIcon(webView: WebView?, icon: Bitmap?) {
@@ -37,7 +40,12 @@ class WebViewFragmentActivity : BaseWebViewActivity() {
             }
 
             override fun onPageFinished(webView: WebView?, url: String?) {
-                webViewFragment?.localStorage("appKcwc", "123")
+                val data = "{\"token\":\"cDaxVHpdMjNvAueIQ3R56svRryk\",\"refreshToken\":\"X8rweH2v7PvsSEr7rEC9ES8vedQ\",\"tokenArray\":{\"tel\":\"13399857800\",\"type\":3,\"source\":\"Pc\"},\"oldtoken\":\"IPNPx563jakGvZrej2FP8IA7yQA58Coh\"}"
+                val j = JSONObject()
+                j.put("source", data)
+                val d = "{source:\"$data\"}"
+                Logger.e(d)
+                webViewFragment?.localStorage("kwcw4-h5", d)
             }
         }
     }
