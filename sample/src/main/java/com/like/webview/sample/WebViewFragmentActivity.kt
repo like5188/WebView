ed8856f9c67db5c4a66fe3465e2cfafe5acab5bb
like5188 <260864866@qq.com>
2022-03-07 19:39:57 +0800

    完善

--- sample/src/main/java/com/like/webview/sample/WebViewFragmentActivity.kt
+++ sample/src/main/java/com/like/webview/sample/WebViewFragmentActivity.kt
@@ -9,8 +9,10 @@ import com.like.webview.BaseWebViewActivity
 import com.like.webview.WebViewFragmentConfig
 import com.like.webview.X5ListenerAdapter
 import com.like.webview.sample.databinding.ActivityWebviewFragmentBinding
+import com.tencent.smtt.sdk.CookieManager
 import com.tencent.smtt.sdk.WebView
 import org.json.JSONObject
+
 
 class WebViewFragmentActivity : BaseWebViewActivity() {
     private val mBinding by lazy {
@@ -27,8 +29,8 @@ class WebViewFragmentActivity : BaseWebViewActivity() {
     }
 
     override fun getWebViewFragmentConfig(): WebViewFragmentConfig = WebViewFragmentConfig().apply {
-        url = "file:///android_asset/index.html"
-//        url = "http://kcwc.m.cacf.cn/static-kcwc2/my/userInfo?client=android"
+//        url = "file:///android_asset/index.html"
+        url = "http://192.168.0.188/my/userInfo?client=android"
         javascriptInterfaceMap["appKcwc"] = JsUtils.JavascriptInterface()
         x5Listener = object : X5ListenerAdapter() {
             override fun onReceivedIcon(webView: WebView?, icon: Bitmap?) {
@@ -39,14 +41,29 @@ class WebViewFragmentActivity : BaseWebViewActivity() {
                 mBinding.tvTitle.text = title
             }
 
-            override fun onPageFinished(webView: WebView?, url: String?) {
-                val data = "{\"token\":\"cDaxVHpdMjNvAueIQ3R56svRryk\",\"refreshToken\":\"X8rweH2v7PvsSEr7rEC9ES8vedQ\",\"tokenArray\":{\"tel\":\"13399857800\",\"type\":3,\"source\":\"Pc\"},\"oldtoken\":\"IPNPx563jakGvZrej2FP8IA7yQA58Coh\"}"
+            override fun onPageStarted(webView: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(webView, url, favicon)
+                // localStorage
+                val data =
+                    "{\"token\":\"cDaxVHpdMjNvAueIQ3R56svRryk\",\"refreshToken\":\"X8rweH2v7PvsSEr7rEC9ES8vedQ\",\"tokenArray\":{\"tel\":\"13399857800\",\"type\":3,\"source\":\"Pc\"},\"oldtoken\":\"IPNPx563jakGvZrej2FP8IA7yQA58Coh\"}"
                 val j = JSONObject()
                 j.put("source", data)
                 val d = "{source:\"$data\"}"
                 Logger.e(d)
-                webViewFragment?.localStorage("kwcw4-h5", d)
+                webViewFragment?.localStorage("kwcw4-h5", j.toString())
+
+                // cookie
+                try {
+                    CookieManager.getInstance().apply {
+                        setAcceptCookie(true)
+                        removeAllCookie()
+                        setCookie("http://192.168.0.188", "token=cDaxVHpdMjNvAueIQ3R56svRryk")
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
+
         }
     }
 
