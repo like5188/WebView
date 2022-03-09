e4090642b997df5f98fc06ebc32ad961b99a4908
like5188 <260864866@qq.com>
2022-03-09 09:51:58 +0800

    完善

--- sample/src/main/java/com/like/webview/sample/MyJavascriptInterface.kt
+++ sample/src/main/java/com/like/webview/sample/MyJavascriptInterface.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import org.json.JSONObject
 
 class MyJavascriptInterface {
+    // 注意线程。如果在方法中操作 WebView，就必须在主线程执行。
+    // 否则会出现：java.lang.Throwable: A WebView method was called on thread 'JavaBridge'. All WebView methods must be called on the same thread. (Expected Looper Looper (main, tid 2) {75a8318} called on Looper (JavaBridge, tid 89) {424971f}, FYI main Looper is Looper (main, tid 2) {75a8318})
     @android.webkit.JavascriptInterface// API17及以上的版本中，需要此注解才能调用下面的方法
     fun androidMethod(params: String): String {
         try {
